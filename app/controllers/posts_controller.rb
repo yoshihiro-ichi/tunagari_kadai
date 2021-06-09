adfd6a0722c76acdb05bfec961d2dcbcc7bafb95
yoshihiro ichikawa <aden.yi21120821@gmail.com>
2021-06-09 14:36:23 +0000

    機能（アクション）を設定しました

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,6 +1,55 @@
 class PostsController < ApplicationController
+before_action :set_post, only: [:show, :edit, :update, :destroy]
   def index
+    @posts = Post.all
   end
+
   def new
+    @post = Post.new
+  end
+
+  def create
+    @post = Post.new(post_params)
+    if params[:back]
+      render :new
+    else
+      if @post.save
+        redirect_to posts_path,notice:"ツイートを作成しました！！"
+      else
+        render :new
+      end
+    end
+  end
+
+  def show
+  end
+
+  def edit
+  end
+
+  def update
+    if @post.update(post_params)
+      redirect_to posts_path
+    else
+      render :edit
+    end
+  end
+
+  def confirm
+    @post = Post.new(post_params)
+  end
+
+  def destroy
+     @post.destroy
+      redirect_to posts_path
+  end
+
+  private
+  def post_params
+    params.require(:post).permit(:content)
+  end
+
+  def set_post
+    @post = Post.find(params[:id])
   end
 end
